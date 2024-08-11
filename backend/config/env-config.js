615f056111148331cc927ae3546462b15927bc2d
MohitCode17 <mohit.codes17@gmail.com>
2024-08-11 12:15:42 +0530

    add cloudinary config

--- backend/config/env-config.js
+++ backend/config/env-config.js
@@ -4,6 +4,9 @@ conf();
 const _config = {
   port: process.env.PORT,
   mongodb_url: process.env.MONGODB_URL,
+  cloudinary_cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  cloudinary_api_key: process.env.CLOUDINARY_API_KEY,
+  cloudinary_api_secret: process.env.CLOUDINARY_API_SECRET,
 };
 
 export const config = Object.freeze(_config);
