27df175fff7dad88a0b38c4b16af6dfb7085253a
MohitCode17 <mohit.codes17@gmail.com>
2024-08-14 22:19:41 +0530

    add get applications route

--- backend/controllers/application.controller.js
+++ backend/controllers/application.controller.js
@@ -53,3 +53,34 @@ export const handleApplyJob = async (req, res) => {
     });
   }
 };
+
+export const handleGetApplications = async (req, res) => {
+  try {
+    const userId = req.id;
+    const applications = await Application.find({ applicant: userId })
+      .sort({ createdAt: -1 })
+      .populate({
+        path: "job",
+        options: { sort: { createdAt: -1 } },
+        populate: {
+          path: "company",
+          options: { sort: { createdAt: -1 } },
+        },
+      });
+    if (!applications)
+      return res.status(404).json({
+        success: false,
+        message: "No Applications",
+      });
+
+    return res.status(200).json({
+      success: true,
+      applications,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message || "Internal Server Error",
+    });
+  }
+};
--- backend/routes/application.route.js
+++ backend/routes/application.route.js
@@ -1,10 +1,13 @@
 import express from "express";
-import { handleApplyJob } from "../controllers/application.controller.js";
+import { handleApplyJob, handleGetApplications } from "../controllers/application.controller.js";
 import { authenticate } from "../middlewares/authenticate.js";
 
 const router = express.Router();
 
 // APPLY JOB ROUTE (FOR JOB-SEEKER)
 router.get("/apply/:id", authenticate, handleApplyJob);
+
+// GET APPLIED JOB APPLICATIONS (FOR JOB-SEEKER)
+router.get("/get", authenticate, handleGetApplications);
 
 export default router;
