1b4813ddb66e98d5d9a05257cedde912b042e67d
MohitCode17 <mohit.codes17@gmail.com>
2024-08-19 10:19:34 +0530

    add update application status functionality

--- frontend/src/components/admin/ApplicantsTable.jsx
+++ frontend/src/components/admin/ApplicantsTable.jsx
@@ -10,10 +10,32 @@ import {
 } from "../ui/table";
 import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 import { MoreHorizontal } from "lucide-react";
+import { toast } from "sonner";
+import axios from "axios";
+import { BACKEND_APPLICATION_URL } from "@/constants/constants";
 
 const shortlistingStatus = ["Accepted", "Rejected"];
 const ApplicantsTable = () => {
   const { applicants } = useSelector((state) => state.application);
+
+  // HANDLE UPDATE STATUS
+  const handleUpdateStatus = async (id, status) => {
+    try {
+      const res = await axios.put(
+        `${BACKEND_APPLICATION_URL}/status/${id}/update`,
+        { status },
+        {
+          withCredentials: true,
+        }
+      );
+
+      if (res.data.success) {
+        toast.error(res.data.message);
+      }
+    } catch (error) {
+      toast.error(error.message || "Error updating status, Try again!");
+    }
+  };
 
   return (
     <div>
@@ -61,6 +83,7 @@ const ApplicantsTable = () => {
                         return (
                           <div
                             key={index}
+                            onClick={() => handleUpdateStatus(item._id, status)}
                             className="flex w-fit items-center my-2 cursor-pointer"
                           >
                             <span>{status}</span>
